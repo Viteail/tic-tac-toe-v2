d3c777ee05ce842d9f0eb6e8452d88fc35d4f32a
Viteail <vitea.sirbu1@gmail.com>
2023-07-29 11:41:39 +0300

    feat: add minimax

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const game = {
-  gameboard: ['', '', '', '', '', '', '', '', ''],
+  gameboard: [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', ''],
+  ],
   players: {
     playerOne: {
       name: '',
@@ -10,13 +14,19 @@ const game = {
       marker: 'O',
     },
   },
+  aiMode: false,
 };
 
 const displayController = (() => {
   const playerOne = document.querySelector('.player-one');
   const playerTwo = document.querySelector('.player-two');
-  const displayMarkers = (gameboardElement) => {
-    game.gameboard.forEach((item, index) => {
+  const displayMarkers = () => {
+    const gameboardElement = document.querySelector('.gameboard');
+    let mergedBoard = game.gameboard[0].concat(
+      game.gameboard[1],
+      game.gameboard[2]
+    );
+    mergedBoard.forEach((item, index) => {
       gameboardElement.children[index].textContent = item;
     });
   };
@@ -64,6 +74,7 @@ const displayController = (() => {
     });
 
     returnButton.addEventListener('click', () => {
+      game.aiMode = false;
       modalWinner.classList.add('invisible');
       gameBoard.restartGame();
       displayStartMenu();
@@ -73,10 +84,20 @@ const displayController = (() => {
   const displayModeSelection = () => {
     const modalSelection = document.querySelector('.modal-selection');
     const pvpButton = document.querySelector('.pvp-mode');
+    const aiButton = document.querySelector('.ai-mode');
     modalSelection.classList.remove('invisible');
+
     pvpButton.addEventListener('click', () => {
       modalSelection.classList.add('invisible');
       displayPlayersModal();
+    });
+
+    aiButton.addEventListener('click', () => {
+      modalSelection.classList.add('invisible');
+      modalSelection.parentElement.classList.add('invisible');
+      game.aiMode = true;
+      gameBoard.namePlayers();
+      showGameBoard();
     });
   };
 
@@ -118,18 +139,51 @@ const gameBoard = (() => {
   const gameboardElement = document.querySelector('.gameboard');
   const attachEvent = () => {
     gameboardElement.addEventListener('click', (e) => {
-      if (
-        game.gameboard[
+      if (checkWinner() !== null) return;
+      if ([Array.from(gameboardElement.children).indexOf(e.target)] < 3) {
+        if (
+          game.gameboard[0][
+            Array.from(gameboardElement.children).indexOf(e.target)
+          ] !== ''
+        )
+          return;
+        game.gameboard[0][
           Array.from(gameboardElement.children).indexOf(e.target)
-        ] !== ''
-      )
-        return;
-      game.gameboard[Array.from(gameboardElement.children).indexOf(e.target)] =
-        currentPlayerTurn.marker;
-      win(currentPlayerTurn);
+        ] = currentPlayerTurn.marker;
+      } else if (
+        [Array.from(gameboardElement.children).indexOf(e.target)] > 2 &&
+        Array.from(gameboardElement.children).indexOf(e.target) < 6
+      ) {
+        if (
+          game.gameboard[1][
+            Array.from(gameboardElement.children).indexOf(e.target) - 3
+          ] !== ''
+        )
+          return;
+        game.gameboard[1][
+          Array.from(gameboardElement.children).indexOf(e.target) - 3
+        ] = currentPlayerTurn.marker;
+      } else if (
+        Array.from(gameboardElement.children).indexOf(e.target) > 5 &&
+        Array.from(gameboardElement.children).indexOf(e.target) < 9
+      ) {
+        if (
+          game.gameboard[2][
+            Array.from(gameboardElement.children).indexOf(e.target) - 6
+          ] !== ''
+        )
+          return;
+        game.gameboard[2][
+          Array.from(gameboardElement.children).indexOf(e.target) - 6
+        ] = currentPlayerTurn.marker;
+      }
+      findWinner();
       switchPlayerTurn();
-      displayController.scalePlayerTurn(currentPlayerTurn);
-      displayController.displayMarkers(gameboardElement);
+
+      if (game.aiMode === false)
+        displayController.scalePlayerTurn(currentPlayerTurn);
+      displayController.displayMarkers();
+      if (game.aiMode === true && checkWinner() === null) findBestMove();
     });
   };
 
@@ -138,33 +192,76 @@ const gameBoard = (() => {
       ? (currentPlayerTurn = game.players.playerTwo)
       : (currentPlayerTurn = game.players.playerOne);
 
-  const win = (player) => {
-    const winCondition = {
-      0: [0, 1, 2],
-      1: [3, 4, 5],
-      2: [6, 7, 8],
-      3: [0, 3, 6],
-      4: [1, 4, 7],
-      5: [2, 5, 8],
-      6: [0, 4, 8],
-      7: [2, 4, 6],
+  const checkWinner = () => {
+    const equals3 = (a, b, c) => {
+      return a === b && b === c && a !== '';
     };
-    let indexes = [];
-    const filtered = game.gameboard.filter((item, index) => {
-      if (item === player.marker) indexes.push(index);
-    });
-    for (let i = 0; i < Object.keys(winCondition).length; i++) {
-      if (winCondition[i].every((value) => indexes.includes(value)))
-        return displayController.displayWinner(`${player.name} has Won!`);
-      else if (game.gameboard.every((value) => value !== ''))
-        return displayController.displayWinner("It's a Tie!");
+    for (let i = 0; i < 3; i++) {
+      if (
+        equals3(
+          game.gameboard[i][0],
+          game.gameboard[i][1],
+          game.gameboard[i][2]
+        )
+      ) {
+        return game.gameboard[i][0];
+      }
     }
-    console.log(indexes, winCondition);
+    for (let i = 0; i < 3; i++) {
+      if (
+        equals3(
+          game.gameboard[0][i],
+          game.gameboard[1][i],
+          game.gameboard[2][i]
+        )
+      ) {
+        return game.gameboard[0][i];
+      }
+    }
+    if (
+      equals3(game.gameboard[0][0], game.gameboard[1][1], game.gameboard[2][2])
+    ) {
+      return game.gameboard[0][0];
+    }
+
+    if (
+      equals3(game.gameboard[2][0], game.gameboard[1][1], game.gameboard[0][2])
+    ) {
+      return game.gameboard[2][0];
+    }
+
+    let mergedBoard = game.gameboard[0].concat(
+      game.gameboard[1],
+      game.gameboard[2]
+    );
+
+    if (mergedBoard.every((elem) => elem !== '')) {
+      return 'tie';
+    }
+    return null;
+  };
+
+  const findWinner = () => {
+    if (checkWinner() === 'X') {
+      displayController.displayWinner(
+        `${game.players.playerOne.name} has Won!`
+      );
+    } else if (checkWinner() === 'O') {
+      displayController.displayWinner(
+        `${game.players.playerTwo.name} has Won!`
+      );
+    } else if (checkWinner() === 'tie') {
+      displayController.displayWinner("It's a Tie!");
+    }
   };
 
   const restartGame = () => {
     const gameboardElement = document.querySelector('.gameboard');
-    game.gameboard = ['', '', '', '', '', '', '', '', ''];
+    game.gameboard = [
+      ['', '', ''],
+      ['', '', ''],
+      ['', '', ''],
+    ];
     currentPlayerTurn = game.players.playerOne;
     displayController.scalePlayerTurn(currentPlayerTurn);
     displayController.displayMarkers(gameboardElement);
@@ -179,7 +276,74 @@ const gameBoard = (() => {
       game.players.playerOne.name = playerOneInput.getAttribute('placeholder');
     if (game.players.playerTwo.name === '')
       game.players.playerTwo.name = playerTwoInput.getAttribute('placeholder');
+    if (game.aiMode === true) {
+      game.players.playerOne.name = 'Player One';
+      game.players.playerTwo.name = 'AI';
+    }
     displayController.displayPlayers();
+  };
+
+  const findBestMove = () => {
+    let bestScore = -Infinity;
+    let bestMove;
+    for (let i = 0; i < 3; i++) {
+      for (let k = 0; k < 3; k++) {
+        if (game.gameboard[i][k] === '') {
+          game.gameboard[i][k] = game.players.playerTwo.marker;
+          let score = minimax(game.gameboard, 0, false);
+          game.gameboard[i][k] = '';
+          if (score > bestScore) {
+            bestScore = score;
+            bestMove = { i, k };
+          }
+        }
+      }
+    }
+    game.gameboard[bestMove.i][bestMove.k] = game.players.playerTwo.marker;
+    displayController.displayMarkers();
+    findWinner();
+    switchPlayerTurn();
+  };
+
+  let scores = {
+    X: -1,
+    O: 1,
+    tie: 0,
+  };
+
+  const minimax = (gameboard, depth, isMaximizing) => {
+    let result = checkWinner();
+    if (result !== null) {
+      return scores[result];
+    }
+
+    if (isMaximizing) {
+      let bestScore = -Infinity;
+      for (let i = 0; i < 3; i++) {
+        for (let k = 0; k < 3; k++) {
+          if (gameboard[i][k] === '') {
+            gameboard[i][k] = game.players.playerTwo.marker;
+            let score = minimax(gameboard, depth + 1, false);
+            gameboard[i][k] = '';
+            bestScore = Math.max(score, bestScore);
+          }
+        }
+      }
+      return bestScore;
+    } else {
+      let bestScore = Infinity;
+      for (let i = 0; i < 3; i++) {
+        for (let k = 0; k < 3; k++) {
+          if (gameboard[i][k] === '') {
+            gameboard[i][k] = game.players.playerOne.marker;
+            let score = minimax(gameboard, depth + 1, true);
+            gameboard[i][k] = '';
+            bestScore = Math.min(score, bestScore);
+          }
+        }
+      }
+      return bestScore;
+    }
   };
 
   return { attachEvent, restartGame, namePlayers };
